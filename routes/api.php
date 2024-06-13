041c821ab78be6abc24df0e99d2b2504bd4b5be6
vKyritsis <v.kyritsis@doitforme.eu>
2024-06-13 23:53:31 +0300

    /location endpoint without chr name

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,8 @@ use App\Http\Controllers\ExpressionsController;
 use App\Http\Controllers\LocalizationsController;
 
 
-Route::get('/LncBaseV4/interactions/keyword', [InteractionController::class, 'show']);
-Route::get('/LncBaseV4/interactions/keyword', [InteractionController::class, 'show']);
+Route::get('/LncBaseV4/interactions/keyword', [InteractionController::class, 'showByKeyword']);
+Route::get('/LncBaseV4/interactions/location', [InteractionController::class, 'showByLocation']);
 Route::get('/LncBaseV4/expressions', [ExpressionsController::class, 'show']);
 Route::get('/LncBaseV4/localizations', [LocalizationsController::class, 'show']);
 Route::get('/LncBaseV4/initial', [InitialCallController::class, 'getAll']);
